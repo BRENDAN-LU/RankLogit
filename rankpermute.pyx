9b3c69b013395e83ee5bd4f65dbc9daa2b7f759b
BLuLaptop <brendannlu5@gmail.com>
2023-07-17 19:32:15 +1000

    write some untested wrapper stuff

--- rankpermute.pyx
+++ rankpermute.pyx
@@ -7,5 +7,17 @@ Cython wrapper for _rankpermute
 import numpy as np
 
 cdef extern from "_rankpermute.c": 
-    double sigma_permute(double *arr, unsigned int len, double D)
+    double sigma_permute(double*, unsigned int, double)
 
+# Python wrapper for backend use in Python module to compute likelihood for 
+# generalized tied rank logit model
+def _sigma_permute(arr, D): 
+
+    # Cast input into appropriate np array
+    cdef double[:] arr_copy = arr.copy().astype(float, order='C')
+
+    # Get the length for our C-level function
+    cdef unsigned int arr_size = len(arr_copy)
+
+    # Call function and return computation value 
+    return sigma_permute(&arr_copy[0], arr_size, D)
